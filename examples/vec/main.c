353c56cbbe48c110ef81c33cdb309187d0f144e9
Ben Wernicke <ben@wernicke.at>
2024-04-15 11:54:01 +0200

    vec working but I think the vec was a stupid idea

---
+++ examples/vec/main.c
@@ -0,0 +1,49 @@
+#include "../../allocator/allocator.h"
+#include "../../vec/vec.h"
+#include <stdio.h>
+
+static void dump_vec(usize len, int vec[len]) {
+  for (usize i = 0; i < len; ++i) {
+    printf("% 4d ", vec[i]);
+    if (i % 10 == 0) {
+      putc('\n', stdout);
+    }
+  }
+  putc('\n', stdout);
+  putc('\n', stdout);
+}
+
+int main(void) {
+  printf("Hello World!\n");
+  Allocator *allocator = allocator_get_global();
+
+  int *vec = vec_create(allocator, int, 8, NULL);
+
+  // reserve some 50 ints worth of space
+  vec_reserve(&vec, allocator, 50, NULL);
+
+  // vec_more makes sure enough space is available increases the length by one
+  // and returns a pointer to the last element
+  for (int i = 0; i < 100; ++i) {
+    *vec_more(&vec, allocator, NULL) = i;
+  }
+
+  // vec_length returns the current length of the vector
+  dump_vec(vec_length(&vec), vec);
+
+  for (int i = 49; i >= 0; --i) {
+    vec_remove(&vec, i * 2);
+  }
+
+  dump_vec(vec_length(&vec), vec);
+
+  for (int i = 0; i < 69; ++i) {
+    *vec_insert(&vec, allocator, 10, NULL) = i;
+  }
+
+  dump_vec(vec_length(&vec), vec);
+
+  vec_destroy(&vec, allocator, NULL);
+
+  return 0;
+}
