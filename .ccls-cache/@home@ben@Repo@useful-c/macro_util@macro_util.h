9abda99e88822863baf15bb0a3e11d9734554ab1
Ben Wernicke <ben@wernicke.at>
2024-04-17 16:44:57 +0200

    ccls cache is not needed

--- .ccls-cache/@home@ben@Repo@useful-c/macro_util@macro_util.h
+++
@@ -1,9 +0,0 @@
-#ifndef MACRO_UTIL_H_
-#define MACRO_UTIL_H_
-
-#define UNUSED(A) (void)(A)
-#define LIKELY(A) (__builtin_expect((A), 1))
-#define UNLIKELY(A) (__builtin_expect((A), 0))
-#define TRAP() __builtin_trap()
-
-#endif // MACRO_UTIL_H_
